ba59eb26bb6730d9080cdd63ffe483b48280ecc9
Steven Rowlands <steven.rowlands@gmail.com>
2013-04-16 20:41:18 +1000

    additional tests for SingleTreeDiffFilter

--- gitistics-core/src/main/java/org/gitistics/treewalk/SingleTreeDiffFilter.java
+++ gitistics-core/src/main/java/org/gitistics/treewalk/SingleTreeDiffFilter.java
@@ -3,10 +3,13 @@ package org.gitistics.treewalk;
 import org.eclipse.jgit.treewalk.TreeWalk;
 
 /**
- * Selects only tree entries where all tree's differ from the single tree at the specified index
+ * Selects only tree entries where all other tree's differ from the single tree at the specified index
  * 
  * <p>
- * This is useful to see which entries were actually changed in a specific tree
+ * This is useful to see which entries were actually changed in a specific tree.
+ * 
+ * An example usage is for a merge commit in determining what paths were actually
+ * modified as part of the merge and weren't just pulled in from a parent tree
  * <p>
  */
 public class SingleTreeDiffFilter extends AbstractTreeFilter {
--- gitistics-core/src/test/java/org/gitistics/treewalk/SingleTreeDiffFilterTest.java
+++ gitistics-core/src/test/java/org/gitistics/treewalk/SingleTreeDiffFilterTest.java
@@ -3,10 +3,13 @@ package org.gitistics.treewalk;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.treewalk.TreeWalk;
+import org.gitective.core.CommitUtils;
 import org.gitective.core.TreeUtils;
 import org.gitistics.AbstractGitTest;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class SingleTreeDiffFilterTest extends AbstractGitTest {
@@ -59,6 +62,61 @@ public class SingleTreeDiffFilterTest extends AbstractGitTest {
 				.get();
 		runSingleParentWithSingleFileReverse(0, commit2);
 		runSingleParentWithSingleFileReverse(1, commit2);
+	}
+	
+	
+	@Test
+	public void testSingleParentWithSingleFileRemoved() throws Exception {
+		RevCommit commit2 = git.add("file1", "A")
+				.commit("commit1")
+				.remove("file1")
+				.commit("commit2")
+				.get();
+		SingleTreeDiffFilter filterA = new SingleTreeDiffFilter(1);
+		TreeWalk walk = TreeUtils.withParents(repo, commit2);
+		walk.setFilter(filterA);
+		assertThat(walk.next(), equalTo(true));
+		assertThat(walk.getPathString(), equalTo("file1"));
+	}
+	
+	/**
+	 * master - merge
+	 * |                \
+	 * commit3 (+file3)   branch1 - commit2 (+file2)
+	 * |                /
+	 * commit1 (+file1)
+     */
+	@Test
+	public void testMultipleParents() throws Exception {
+		git.add("file1", "A");
+		git.commit("commit1");
+		git.branch("branch1");
+		git.checkout("master");
+		git.add("file2", "A");
+		git.commit("commit2");
+		git.checkout("branch1");
+		git.add("file3", "A"); 
+		RevCommit commit3 = git.commit("commit3").get();
+		git.checkout("master");
+		MergeResult merge = git.merge(commit3).get();
+		
+		SingleTreeDiffFilter filter = new SingleTreeDiffFilter(2);
+		
+		// The merge has nothing interesting
+		TreeWalk walk = TreeUtils.withParents(repo, CommitUtils.getCommit(repo, merge.getNewHead()));
+		walk.setFilter(filter);
+		assertThat(walk.next(), equalTo(false));
+		
+		// The parents relative to each other will have +file2 or +file3
+		// I.e. If commit3 is chosen we will get +file2 as +file2 is added
+		// in both the other trees
+		filter = new SingleTreeDiffFilter(1);
+		walk = TreeUtils.withParents(repo, CommitUtils.getCommit(repo, merge.getNewHead()));
+		walk.setFilter(filter);
+		assertThat(walk.next(), equalTo(true));
+		assertThat(walk.getPathString(), Matchers.anyOf(equalTo("file2"), equalTo("file3")));
+		assertThat(walk.next(), equalTo(false));
+		
 	}
 	
 	
